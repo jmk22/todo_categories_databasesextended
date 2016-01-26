584bd0fb4e5ffbb6871043eb71e2c3afe7bf7a2a
Jill Kuchman <pillanoid@gmail.com>
2016-01-26 10:44:02 -0800

    Add task AddCategory and GetCategories methods with passing unit tests

--- Tests/TaskTest.cs
+++ Tests/TaskTest.cs
@@ -97,28 +97,68 @@ namespace ToDoList
     //   Assert.Equal(newDescription, result);
     // }
 
+    // [Fact]
+    // public void Test_Delete_DeletesTaskFromDatabase()
+    // {
+    //   //Arrange
+    //   string description1 = "Mow the lawn";
+    //   Task testTask1 = new Task(description1);
+    //   testTask1.Save();
+    //
+    //   string description2 = "Walk the dog";
+    //   Task testTask2 = new Task(description2);
+    //   testTask2.Save();
+    //
+    //   //Act
+    //   testTask1.Delete();
+    //   List<Task> resultTasks = Task.GetAll();
+    //   List<Task> testTaskList = new List<Task> {testTask2};
+    //
+    //   //Assert
+    //   Assert.Equal(testTaskList, resultTasks);
+    // }
+
     [Fact]
-    public void Test_Delete_DeletesTaskFromDatabase()
+    public void Test_AddCategory_AddsCategoryToTask()
     {
       //Arrange
-      string description1 = "Mow the lawn";
-      Task testTask1 = new Task(description1);
-      testTask1.Save();
+      Task testTask = new Task("Mow the lawn");
+      testTask.Save();
 
-      string description2 = "Walk the dog";
-      Task testTask2 = new Task(description2);
-      testTask2.Save();
+      Category testCategory = new Category("Home stuff");
+      testCategory.Save();
 
       //Act
-      testTask1.Delete();
-      List<Task> resultTasks = Task.GetAll();
-      List<Task> testTaskList = new List<Task> {testTask2};
+      testTask.AddCategory(testCategory);
+
+      List<Category> result = testTask.GetCategories();
+      List<Category> testList = new List<Category>{testCategory};
 
       //Assert
-      Assert.Equal(testTaskList, resultTasks);
+      Assert.Equal(testList, result);
     }
 
+        [Fact]
+        public void Test_GetCategories_ReturnsAllTaskCategories()
+        {
+          //Arrange
+          Task testTask = new Task("Mow the lawn");
+          testTask.Save();
 
+          Category testCategory1 = new Category("Home stuff");
+          testCategory1.Save();
+
+          Category testCategory2 = new Category("Work stuff");
+          testCategory2.Save();
+
+          //Act
+          testTask.AddCategory(testCategory1);
+          List<Category> result = testTask.GetCategories();
+          List<Category> testList = new List<Category> {testCategory1};
+
+          //Assert
+          Assert.Equal(testList, result);
+        }
 
     public void Dispose()
     {
