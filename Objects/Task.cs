1c55d45d6b91652de6524d1431aefd2e483eb00b
Jill Kuchman <pillanoid@gmail.com>
2016-01-25 19:15:25 -0800

    Initial commit - add initial files with AddTasks and GetTasks methods for Category

---
+++ Objects/Task.cs
@@ -0,0 +1,146 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace ToDoList
+{
+  public class Task
+  {
+    private int id;
+    private string description;
+
+    public Task(string Description, int Id = 0)
+    {
+      id = Id;
+      description = Description;
+    }
+
+    public override bool Equals(System.Object otherTask)
+    {
+        if (!(otherTask is Task))
+        {
+          return false;
+        }
+        else {
+          Task newTask = (Task) otherTask;
+          bool idEquality = this.GetId() == newTask.GetId();
+          bool descriptionEquality = this.GetDescription() == newTask.GetDescription();
+          return (idEquality && descriptionEquality);
+        }
+    }
+
+
+    public int GetId()
+    {
+      return id;
+    }
+    public string GetDescription()
+    {
+      return description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      description = newDescription;
+    }
+
+    public static List<Task> GetAll()
+    {
+      List<Task> AllTasks = new List<Task>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int taskId = rdr.GetInt32(0);
+        string taskDescription = rdr.GetString(1);
+        Task newTask = new Task(taskDescription, taskId);
+        AllTasks.Add(newTask);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return AllTasks;
+    }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO tasks (description) OUTPUT INSERTED.id VALUES (@TaskDescription)", conn);
+
+      SqlParameter descriptionParam = new SqlParameter();
+      descriptionParam.ParameterName = "@TaskDescription";
+      descriptionParam.Value = this.GetDescription();
+
+      cmd.Parameters.Add(descriptionParam);
+
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this.id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM tasks;", conn);
+      cmd.ExecuteNonQuery();
+    }
+
+    public static Task Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks WHERE id = @TaskId", conn);
+      SqlParameter taskIdParameter = new SqlParameter();
+      taskIdParameter.ParameterName = "@TaskId";
+      taskIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(taskIdParameter);
+      rdr = cmd.ExecuteReader();
+
+      int foundTaskId = 0;
+      string foundTaskDescription = null;
+
+      while(rdr.Read())
+      {
+        foundTaskId = rdr.GetInt32(0);
+        foundTaskDescription = rdr.GetString(1);
+      }
+      Task foundTask = new Task(foundTaskDescription, foundTaskId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundTask;
+    }
+  }
+}
---
+++ Tests/TaskTest.cs
@@ -0,0 +1,89 @@
+// using Xunit;
+// using System.Collections.Generic;
+// using System;
+// using System.Data;
+// using System.Data.SqlClient;
+//
+// namespace ToDoList
+// {
+//   public class TaskTest : IDisposable
+//   {
+//     public TaskTest()
+//     {
+//       DBConfiguration.connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo_extended_test;Integrated Security=SSPI;";
+//     }
+//
+//     [Fact]
+//     public void Test_EmptyAtFirst()
+//     {
+//       //Arrange, Act
+//       int result = Task.GetAll().Count;
+//
+//       //Assert
+//       Assert.Equal(0, result);
+//     }
+//
+//     [Fact]
+//     public void Test_EqualOverrideTrueForSameDescription()
+//     {
+//       //Arrange, Act
+//       Task firstTask = new Task("Mow the lawn");
+//       Task secondTask = new Task("Mow the lawn");
+//
+//       //Assert
+//       Assert.Equal(firstTask, secondTask);
+//     }
+//
+//     [Fact]
+//     public void Test_Save()
+//     {
+//       //Arrange
+//       Task testTask = new Task("Mow the lawn");
+//       testTask.Save();
+//
+//       //Act
+//       List<Task> result = Task.GetAll();
+//       List<Task> testList = new List<Task>{testTask};
+//
+//       //Assert
+//       Assert.Equal(testList, result);
+//     }
+//
+//     [Fact]
+//     public void Test_SaveAssignsIdToObject()
+//     {
+//       //Arrange
+//       Task testTask = new Task("Mow the lawn");
+//       testTask.Save();
+//
+//       //Act
+//       Task savedTask = Task.GetAll()[0];
+//
+//       int result = savedTask.GetId();
+//       int testId = testTask.GetId();
+//
+//       //Assert
+//       Assert.Equal(testId, result);
+//     }
+//
+//     [Fact]
+//     public void Test_FindFindsTaskInDatabase()
+//     {
+//       //Arrange
+//       Task testTask = new Task("Mow the lawn");
+//       testTask.Save();
+//
+//       //Act
+//       Task foundTask = Task.Find(testTask.GetId());
+//
+//       //Assert
+//       Assert.Equal(testTask, foundTask);
+//     }
+//
+//     public void Dispose()
+//     {
+//       Task.DeleteAll();
+//       Category.DeleteAll();
+//     }
+//   }
+// }
